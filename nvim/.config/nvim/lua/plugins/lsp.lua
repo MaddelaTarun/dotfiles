7c0166371effb73271206d0684f64551708eeb04
Tarun <maddelatarun0@gmail.com>
2026-02-25 06:32:24 +0530

    Minor

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,55 +4,70 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",         -- The actual completion engine (THE MISSING PIECE)
-      "hrsh7th/cmp-nvim-lsp",     -- The bridge between LSP and completion
-      "L3MON4D3/LuaSnip",         -- Required snippet engine for the menu to work
+      "hrsh7th/nvim-cmp",         -- Autocomplete Engine
+      "hrsh7th/cmp-nvim-lsp",     -- Bridge for LSP
+      "hrsh7th/cmp-buffer",       -- Complete from current file
+      "hrsh7th/cmp-path",         -- Complete file paths
+      "L3MON4D3/LuaSnip",         -- Snippets (Required)
+      "saadparwaiz1/cmp_luasnip", 
     },
     config = function()
       -- 1. Setup Mason
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "jdtls", "lua_ls" },
+        -- Ensure these are automatically installed
+        ensure_installed = { "clangd", "jdtls", "lua_ls", "ts_ls" }, 
       })
 
-      -- 2. Setup Autocompletion (Fixes your 'cmp' not found error)
+      -- 2. Setup Autocompletion (CMP)
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
       cmp.setup({
         snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
+          expand = function(args) luasnip.lsp_expand(args.body) end,
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ["<C-Space>"] = cmp.mapping.complete(), -- Trigger dropdown
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Enter to accept
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-        })
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
       })
 
-      -- 3. Define LSP Settings (The Native 0.11+ Way)
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- 3. Define and Enable LSPs (The Neovim 0.11 Way)
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Lua
-      vim.lsp.config('lua_ls', {
+      -- Configuration for JavaScript / TypeScript
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+        -- Default filetypes for JS/TS
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      })
+
+      -- Configuration for Lua (Your Config)
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
         settings = { Lua = { diagnostics = { globals = { "vim" } } } }
       })
 
-      -- C/C++
-      vim.lsp.config('clangd', { capabilities = capabilities })
+      -- Configuration for C/C++
+      vim.lsp.config("clangd", { capabilities = capabilities })
 
-      -- Java
-      vim.lsp.config('jdtls', { capabilities = capabilities })
+      -- ACTIVATE THE SERVERS
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("clangd")
 
-      -- 4. Enable the servers
-      vim.lsp.enable('lua_ls')
-      vim.lsp.enable('clangd')
-      vim.lsp.enable('jdtls')
+      -- Recommended Global Keybinds
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "LSP Hover Documentation" })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "LSP Go to Definition" })
     end,
   },
 }
